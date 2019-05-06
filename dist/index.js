b2c6bbb27b5fde465240519e06d64652a2a05871
jake <kenshinhm@naver.com>
2019-05-06 23:48:01 +0900

    types and tsc-watch setup

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Nicolas", age = 24, gender = "male";
 const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
 };
-sayHi(name, age);
+console.log(sayHi("Nicolas", 444, "female"));
 //# sourceMappingURL=index.js.map
